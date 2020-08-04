d73c837c6297ae7e8ca10c160c7d3a40b3b7e062
vagrant <vagrant@localhost.localdomain>
2020-08-04 03:13:41 +0000

    route error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
   #論理削除のため、コード追加
-  devise_for :customers :controllers => { :registrations => 'customers/registrations' }
+  devise_for :customers, :controllers => { :registrations => 'customers/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #管理者側
@@ -14,14 +14,14 @@ Rails.application.routes.draw do
   end
   #顧客側
   root 'homes#top'
-  get 'home/about' => 'homes#about', as 'about'
+  get 'homes/about' => 'homes#about', to: 'about'
   resources :products, only: [:index, :show]
   resources :cart_items, except: [:show, :edit, :new]
   resource :orders, only: [:new, :create]  do
-  	get 'orders' => 'orders#top', as 'orders'
+  	get 'orders' => 'orders#top', to: 'orders'
   end
   resource :customers, only: [:show, :edit, :update]  do
-  	get 'customer/resign' => 'customer#resign', as 'resign'
+  	get 'customer/resign' => 'customer#resign', to: 'resign'
   end
   resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
   resources :order_details, only: [:index, :show]
