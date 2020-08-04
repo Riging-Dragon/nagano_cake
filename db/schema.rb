539b1761deaa7724cc9ad867ff874759cf2897f3
vagrant <vagrant@localhost.localdomain>
2020-08-04 09:36:37 +0000

    customer controller 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_094546) do
+ActiveRecord::Schema.define(version: 2020_08_04_092057) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_08_03_094546) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "costomers", force: :cascade do |t|
+  create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -40,11 +40,6 @@ ActiveRecord::Schema.define(version: 2020_08_03_094546) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_costomers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_costomers_on_reset_password_token", unique: true
-  end
-
-  create_table "customers", force: :cascade do |t|
     t.boolean "is_enabled"
     t.string "last_name_kanji"
     t.string "first_name_kanji"
@@ -53,8 +48,8 @@ ActiveRecord::Schema.define(version: 2020_08_03_094546) do
     t.string "zip_code"
     t.string "address"
     t.string "phone_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "deliveries", force: :cascade do |t|
