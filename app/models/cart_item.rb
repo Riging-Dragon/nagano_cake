f24f91d2e4cce2b343e306cc3029ae0c42e6cb45
vagrant <vagrant@localhost.localdomain>
2020-08-10 10:45:53 +0000

    fix

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,10 +2,4 @@ class CartItem < ApplicationRecord
 	belongs_to :customer
 	belongs_to :product
 
-  # 小計
-  def subtotal_price
-	(count * product.non_taxed_price * Constants::TAX).round
-  end
-  # 合計
-
 end
